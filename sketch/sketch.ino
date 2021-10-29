9584f466a5e0f09cd40a9621ffe625835c945467
Caik de Moraes Souza <caikdemoraes@gmail.com>
2021-10-29 20:37:30 -0300

    correção cálculo temperatura

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,23 +1,18 @@
-#include <Stepper.h> 
-
-const int stepsPerRevolution = 200;
-Stepper myStepper(stepsPerRevolution, D0,D1,D2,D3); 
-
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  myStepper.setSpeed(100);
+  //myStepper.setSpeed(100);
   Serial.begin(9600);
 }
 
 void loop() {
   float temp = getTemperature();
-//  Serial.print("Temperatura = ");
-//  Serial.print(temp);
-//  Serial.println("\xC2\xB0");
-  Serial.println("Girando");
-  myStepper.step(5000);
-  Serial.println("terminou");
-  delay(2000);
+  Serial.print("Temperatura = ");
+  Serial.print(temp);
+  Serial.println("\xC2\xB0");
+  //Serial.println("Girando");
+  //myStepper.step(5000);
+  //Serial.println("terminou");
+  delay(1000);
 }
 
 float getTemperature(){
