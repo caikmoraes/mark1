99f0ebf7e6846b17e948f76acd044ff1c83f03d2
Caik de Moraes Souza <caikdemoraes@gmail.com>
2021-10-30 21:15:33 -0300

    Sensor de luminosidade

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,23 +1,23 @@
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  //myStepper.setSpeed(100);
-  Serial.begin(9600);
+int pinoSensorLuz = A0;               
+int valorLuz = 0;            
+ 
+void setup()
+{
+     Serial.begin(9600);
+     pinMode(LED_BUILTIN, OUTPUT);     
 }
-
-void loop() {
-  float temp = getTemperature();
-  Serial.print("Temperatura = ");
-  Serial.print(temp);
-  Serial.println("\xC2\xB0");
-  //Serial.println("Girando");
-  //myStepper.step(5000);
-  //Serial.println("terminou");
-  delay(1000);
-}
-
-float getTemperature(){
-  int reading = analogRead(A0);  
-  float voltage = reading * (3300 / 1024);
-  float temperatureC = (voltage - 500) / 10; 
-  return temperatureC;
+ 
+void loop()
+{
+  valorLuz = analogRead(pinoSensorLuz);     
+  Serial.println(valorLuz);
+  if(valorLuz < 1010)
+  {                
+    digitalWrite(LED_BUILTIN,LOW);
+  }
+  else
+  {                    
+    digitalWrite(LED_BUILTIN,HIGH);
+  }
+  delay(100);                   
 }
